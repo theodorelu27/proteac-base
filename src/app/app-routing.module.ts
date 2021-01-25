98a39c568cba181d666079d11899991b15efec92
spvmastern <theodore.lumaksono@yahoo.com>
2021-01-25 23:36:50 +0100

    Update Content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,7 +10,7 @@ import { BbcComponent } from './pages/bbc/bbc.component';
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'bs-battle-club', component: BbcComponent },
-  { path: 'bs-battle-club/events/guardian-of-the-alex-tournament', component: EventsComponent },
+  { path: 'bs-battle-club/events/singles-day-tournament', component: EventsComponent },
   { path: 'tea-time-gaming', component: TtgComponent },
   { path: 'tea-time-gaming/world-clash', component: WorldclashComponent },
   { path: 'sabd', component: SandboxComponent },
