dc4909c316e1ee36f92c33cbbf08d64ed2af65dc
thlum001 <theodore.lumaksono001@stud.fh-dortmund.de>
2024-09-03 19:13:13 +0200

    Fix design and JS problems

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgbCollapseModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  isMenuCollapsed = true;
 }
