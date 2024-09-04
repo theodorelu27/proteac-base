1db0767305c9c75ffb661f7063e94b55c227cdc5
thlum001 <theodore.lumaksono001@stud.fh-dortmund.de>
2024-09-04 04:57:38 +0200

    Update Profile Content

--- src/app/pages/tea-time-gaming/tea-time-gaming.component.ts
+++ src/app/pages/tea-time-gaming/tea-time-gaming.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-tea-time-gaming',
   standalone: true,
-  imports: [],
+  imports: [NgbAccordionModule],
   templateUrl: './tea-time-gaming.component.html',
   styleUrl: './tea-time-gaming.component.scss'
 })
 export class TeaTimeGamingComponent {
-
 }
